65ece8c442d9131a60666ef96fa18c1045f05276
sondrdesigns <studio@sondrdesigns.com>
2025-11-24 12:58:01 -1000

    Clean up duplicate TypeScript image declarations

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,46 +1,31 @@
 /// <reference types="vite/client" />
 
 declare module '*.png' {
-  const src: string;
-  export default src;
+  const value: string;
+  export default value;
 }
 
 declare module '*.jpg' {
-  const src: string;
-  export default src;
+  const value: string;
+  export default value;
 }
 
 declare module '*.jpeg' {
-  const src: string;
-  export default src;
+  const value: string;
+  export default value;
+}
+
+declare module '*.JPG' {
+  const value: string;
+  export default value;
 }
 
 declare module '*.svg' {
-  const src: string;
-  export default src;
+  const value: string;
+  export default value;
 }
 
 declare module '*.webp' {
-  const src: string;
-  export default src;
-}
-
-declare module '*.png' {
-  const value: string;
-  export default value;
-}
-
-declare module '*.jpg' {
-  const value: string;
-  export default value;
-}
-
-declare module '*.jpeg' {
-  const value: string;
-  export default value;
-}
-
-declare module '*.svg' {
   const value: string;
   export default value;
 }
